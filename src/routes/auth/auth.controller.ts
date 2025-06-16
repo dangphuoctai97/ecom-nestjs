c0e5e59e5a9e9e24d4baf37590122b870c0966a0
Dang Phuoc Tai <dangphuoctai1997@gmail.com>
2025-06-16 09:59:07 +0700

    Tạo decorator @IsPublic

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import {
   SendOTPBodyDTO,
 } from 'src/routes/auth/auth.dto'
 import { AuthService } from 'src/routes/auth/auth.service'
+import { IsPublic } from 'src/shared/decorators/auth.decorator'
 import { UserAgent } from 'src/shared/decorators/userAgent.decorator'
 import { MessageResDTO } from 'src/shared/dtos/response.dto'
 
@@ -18,18 +19,21 @@ import { MessageResDTO } from 'src/shared/dtos/response.dto'
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('register')
+  @IsPublic()
   @ZodSerializerDto(RegisterResDTO)
   register(@Body() body: RegisterBodyDTO) {
     return this.authService.register(body)
   }
 
   @Post('otp')
+  @IsPublic()
   @ZodSerializerDto(MessageResDTO)
   sendOTP(@Body() body: SendOTPBodyDTO) {
     return this.authService.sendOTP(body)
   }
 
   @Post('login')
+  @IsPublic()
   @ZodSerializerDto(LoginResDTO)
   login(@Body() body: LoginBodyDTO, @UserAgent() userAgent: string, @Ip() ip: string) {
     return this.authService.login({
@@ -40,6 +44,7 @@ export class AuthController {
   }
 
   @Post('refresh-token')
+  @IsPublic()
   @HttpCode(HttpStatus.OK)
   @ZodSerializerDto(RefreshTokenResDTO)
   refreshToken(@Body() body: RefreshTokenBodyDTO, @UserAgent() userAgent: string, @Ip() ip: string) {
